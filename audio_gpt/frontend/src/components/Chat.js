e5627b42102e8921a82f0399bc81cdd4ef7f9161
mchoccie <minijm06@gmail.com>
2024-07-29 15:41:55 -0700

    added modal

--- audio_gpt/frontend/src/components/Chat.js
+++ audio_gpt/frontend/src/components/Chat.js
@@ -1,6 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import bot from "../../static/frontend/static/images/bot.png";
 import user from "../../static/frontend/static/images/user.png";
+import Modal from './Modal';
 //import "./Chat.css";
 
 const Message = ({ role, content }) => (
@@ -58,6 +59,7 @@ export default function Chat() {
   const [history, setHistory] = useState([]);
   const messagesEndRef = useRef(null);
   const [loading, setLoading] = useState(false);
+  const [isModalOpen, setModalOpen] = useState(false);
   
   const addMessage = (newMessage) => {
     setMessages(prevMessages => [...prevMessages, newMessage]);
@@ -123,7 +125,10 @@ export default function Chat() {
         <div className="center flex flex-col bg-vanilla rounded-lg shadow-md p-4 w-3/4 h-[80vh]">
           <div className="flex flex-row justify-between items-center">
             <h3 className="text-lg font-semibold">CHAT MESSAGES</h3>
-            <button className="ml-2 px-2 py-2 bg-brown text-white rounded-lg hover:bg-raisin">Template</button>
+            <button className="ml-2 px-2 py-2 bg-brown text-white rounded-lg hover:bg-raisin"
+            onClick={() => setModalOpen(true)}>
+              Template
+            </button>
           </div>
           <hr className="border-t-2 border-amethyst mb-2 mt-2" />
           <div className="overflow-y-auto flex-1 overflow-y-auto mb-4">
@@ -142,6 +147,10 @@ export default function Chat() {
           <Clear onClick={clearChat} className=" w-15 h-15 my-6" />
         </div>
       </div>
+      <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)}>
+        <h2 className="text-xl font-bold">Modal Title</h2>
+        <p>This is the content of the modal.</p>
+      </Modal>
     </div>
   );
 }
