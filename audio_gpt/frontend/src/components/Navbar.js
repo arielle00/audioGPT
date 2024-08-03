5c137bbd0ae602e657bdeea660256542543281a5
mchoccie <minijm06@gmail.com>
2024-08-03 00:20:34 -0700

    navbar to tailwind

--- audio_gpt/frontend/src/components/Navbar.js
+++ audio_gpt/frontend/src/components/Navbar.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import './Navbar.css';
+
 
 export default function Navbar() {
     return (
-        <nav className="nav">
-            <ul>
-                <li>
-                    <a href="/" className="site-title">Home</a>
+        <nav className="bg-[#333] overflow-hidden">
+            <ul className="list-none m-0 p-0 overflow-hidden">
+                <li className='class="text-xl font-bold"'>
+                    <a href="/" className="float-left block text-[#f2f2f2] text-center px-4 py-3.5 no-underline hover:bg-[#ddd] hover:text-black">Home</a>
                 </li>
-                <li>
-                    <a href="/chat">Chat</a>
+                <li className='class="text-xl font-bold"'>
+                    <a href="/chat" className="float-left block text-[#f2f2f2] text-center px-4 py-3.5 no-underline hover:bg-[#ddd] hover:text-black">Chat</a>
                 </li>
             </ul>
         </nav>
